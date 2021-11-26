831ebbd47eedfbc946802b0311e232ba1433df07
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2021-11-26 10:45:05 -0300

    Criação: desenvolvimento da animação do menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 function menu() 
 {
     var mobile = document.querySelector('.menu-mobile')
+    var menu = document.querySelector('.toggle')
+
     mobile.classList.toggle('active')
+    menu.classList.toggle('active')
 }
